f7be94cd89ac953b91ccb3180d6375157490b388
tgrounds <tgrounds@student.21-school.ru>
2022-03-19 15:21:37 +0300

    stack dote without operators overload

---
+++ Makefile
@@ -0,0 +1,35 @@
+FT_NAME	=	ft_containers
+
+FOLDER	=	./
+
+SRC_LIST	=	ft_main.cpp
+
+HDRS_LIST	=	vector.hpp map.hpp stack.hpp set.hpp
+
+SRC	=	$(addprefix $(FOLDER), $(SRC_LIST))
+
+HDRS	=	$(addprefix $(FOLDER), $(HDRS_LIST))
+
+OBJ	=	$(patsubst, %.cpp, %.o, $(SRC))
+
+CC	=	c++
+
+CFLAGS	=	-Wall -Wextra -Werror -std=c++98 -pedantic-errors
+
+.PHONY:	all clean fclean re
+
+all:	$(FT_NAME)
+
+$(FT_NAME):	Makefile $(OBJ) $(HDRS)
+			$(CC) $(CFLAGS) $(HDRS) $(OBJ) -o $(FT_NAME)
+
+%.o:	%.cpp $(HDRS)
+		$(CC) $(CFLAGS) $(HDRS) -o $@ -c $<
+
+clean:
+		rm -f $(OBJ)
+
+fclean: clean
+		rm -f $(NAME_VEC) $(NAME_STACK) $(NAME_MAP) $(NAME_SET)
+
+re:	fclean all
---
+++ stack.hpp
@@ -0,0 +1,25 @@
+#include <iostream.h>
+
+namespace ft
+{
+	template <typename T, typename Container = std::deque >
+	class stack<T>
+	{
+	protected:
+		Container C;
+	private:
+		typedef Container container_type;
+		typedef typename Container::value_type value_type;
+		typedef typename Container::size_type size_type;
+//		typedef typename Container::reference reference;
+//		typedef typename Container::const_reference const_reference;
+	public:
+		explicit stack (const container_type& ctnr = container_type()) : C(cntr) {}
+		bool empty() const { return (C.empty()); }
+		size_type size() const { return (C.size()); }
+		value_type& top() { return (C.back()); }
+		const value_type& top() const { return (C.back()); }
+		void push (const value_type& val) { C.push_back(); }
+		void pop() { C.pop_back(); }
+	};
+}
